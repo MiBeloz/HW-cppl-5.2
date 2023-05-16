891be788f079ce78e9e4bcb7316b5cf4f62d7387
MiBeloz <miha_bmv_@mail.ru>
2023-05-16 16:33:23 +0600

    Initial commit

---
+++ class/Table.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+
+template<class T>
+class Table {
+public:
+	Table(const size_t row, const size_t col);
+
+	T*& operator[](const size_t i);
+	const T*& operator[](const size_t i) const;
+	
+	size_t size() const;
+	
+	~Table();
+	
+private:
+	size_t m_col;
+	size_t m_row;
+	T** m_array;
+};
+
+
+template<class T>
+Table<T>::Table(const size_t row, const size_t col) {
+	if (row > ULONG_MAX || col > ULONG_MAX) {
+		throw std::runtime_error("table is too long");
+	}
+	m_row = row;
+	m_col = col;
+	m_array = new T*[m_row]{};
+	for (size_t i = 0; i < m_row; ++i) {
+		m_array[i] = new T[m_col]{};
+	}
+}
+
+template<class T>
+T*& Table<T>::operator[](const size_t i) {
+	if (i >= m_row) {
+		throw std::runtime_error("out of range");
+	}
+	return m_array[i];
+}
+
+template<class T>
+const T*& Table<T>::operator[](const size_t i) const {
+	std::cout << "const";
+	return m_array[i];
+}
+
+template<class T>
+size_t Table<T>::size() const {
+	return m_row * m_col;
+}
+
+template<class T>
+Table<T>::~Table() {
+	for (size_t i = 0; i < m_row; ++i) {
+		delete[] m_array[i];
+	}
+	delete[] m_array;
+}
---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#include "class/Table.h"
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "Таблица\n" << std::endl;
+	
+	try {
+		size_t rows = 3;
+		size_t cols = 6;
+		Table<int> table(rows, cols);
+
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				table[i][j] = (i + 1) * (j + 1);
+			}
+		}
+
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				std::cout << table[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+
+		std::cout << "Всего элементов: " << table.size() << std::endl;
+	}
+	catch (std::runtime_error& err) {
+		std::cout << err.what() << std::endl;
+	}
+
+	system("pause > nul");
+	return 0;
+}
